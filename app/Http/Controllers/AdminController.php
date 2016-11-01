e389f6086d6324ca05a25e6470a49d8cd6905fa2
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2016-11-01 16:03:29 +0100

    changed to materialize css

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Myths\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
---
+++ tests/RolesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Myths\Role;
+
+class RolesTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function a_new_role_can_be_added_without_description()
+    {
+        $role = Role::create(['name' => 'Role']);
+
+        $this->seeInDatabase('roles', [
+            'id' => $role->id,
+            'name' => $role->name,
+            'description' => null
+        ]);
+    }
+
+    /** @test */
+    public function a_new_role_can_be_added_with_description()
+    {
+        $role = Role::create(['name' => 'Role', 'description' => 'this is a role only for testing']);
+
+        $this->seeInDatabase('roles', [
+            'id' => $role->id,
+            'name' => $role->name,
+            'description' => 'this is a role only for testing'
+        ]);
+    }
+
+}
