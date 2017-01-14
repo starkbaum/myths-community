88ecadffebe62d9cb1824f778d8c373d8753cf53
Stefan Starkbaum <starkbaum.stefan@gmail.com>
2017-01-14 18:33:46 +0100

    added configuration fpr phpunit to use own sqlite database for testing

--- resources/views/forums/show.blade.php
+++ resources/views/forums/show.blade.php
@@ -7,7 +7,7 @@
                 <div class="container">
                     <div class="col s12">
                         <a href="{{ route('forum.index') }}" class="breadcrumb">Forum</a>
-                        <a href="{{ route('forum.index') }}" class="breadcrumb disabled">{{ $category->name }}</a>
+                        <a href="{{ route('forum.index') }}" class="breadcrumb">{{ $category->name }}</a>
                     </div>
                 </div>
             </div>
@@ -21,7 +21,12 @@
             @include('forums.partials.categoriesList')
         </div>
         <div class="col-sm-9">
-
+            <div class="collection">
+                <a href="#!" class="collection-item">Alvin</a>
+                <a href="#!" class="collection-item active">Alvin</a>
+                <a href="#!" class="collection-item">Alvin</a>
+                <a href="#!" class="collection-item">Alvin</a>
+            </div>
         </div>
     </div>
 @endsection
